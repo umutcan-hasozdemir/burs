c0a2a708e636cfc91fbd0fa0233cc35600097779
canumut05 <hasozdemirumutcan@gmail.com>
2024-01-14 17:46:28 +0300

    Proje dosyası ekle.

---
+++ burs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace or096
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] burslar = { 150, 600, 700, 450, 850, 275, 950, 1500, 210, 368 };
+
+            int b = 0;
+
+            foreach (double x in burslar)
+            {
+                if (x < 500)
+                {
+                    burslar[b] += burslar[b] * 0.1 + 50;
+
+                }
+
+                Console.WriteLine(burslar[b]);
+
+                b++;
+            }
+        }
+    }
+}
